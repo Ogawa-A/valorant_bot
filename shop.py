d1c3e68a83dab7c70e687347da8971ad52e148fb
s1eamed <uncle.sum1961@gmail.com>
2022-04-11 07:47:43 +0900

    get dict value

--- shop.py
+++ shop.py
@@ -93,10 +93,9 @@ def get_night_data(rso):
 
       print(offer_data['Offer']['Cost'])
       print(type(offer_data['Offer']['Cost']))
-      print(offer_data['Offer']['Cost'][1])
-      base_cost = offer_data['Offer']['Cost']
+      base_cost = offer_data['Offer']['Cost'].values()[0]
       discount_per = offer_data['DiscountPercent']
-      cost = offer_data['DiscountCosts']
+      cost = offer_data['DiscountCosts'].values()[0]
 
       cost_text = '{0} ({1}:{2}% OFF)'.format(cost, base_cost, discount_per)
       offer_skin_data.append([display_name, cost_text, display_icon])
