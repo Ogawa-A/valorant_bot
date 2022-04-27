01a4e12168616d95367ed5363f9affbdaaeb8d7c
s1eamed <uncle.sum1961@gmail.com>
2022-04-27 20:08:29 +0900

    print message attribute

--- valorant_bot.py
+++ valorant_bot.py
@@ -20,7 +20,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  print(message.content)
+  print(message.activity)
+  print(message.interaction)
+  print(message.raw_mentions)
+
   if message.author.bot or message.type == 'reply':
       return
   if client.user in message.mentions:
